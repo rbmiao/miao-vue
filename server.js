093ebd4689a3e4fb221d39f01e6d3745b73fbe2a
Daniel Miao <rbmiao@gmail.com>
2021-04-06 19:50:13 -0400

    Store source vue code in github. Deploy the code to Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+// sever files from our dist directory which now contains our index.html file
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
+
+console.log('Listening on port: ' + port)
